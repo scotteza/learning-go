a4635835236ebc8fa9b5a5baabab4f11a4b92456
Scott Edwards <scotteza@gmail.com>
2025-02-19 11:23:12 +0000

    Keep learning the basics

--- the_basics.go
+++ the_basics.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var c, python, java bool
+
 func main() {
 	fmt.Println("Hello, everyone")
 
@@ -17,8 +19,36 @@ func main() {
 	fmt.Println(math.Pi)
 
 	fmt.Println(add(42, 13))
+
+	fmt.Println(add_many(1, 2, 3, 4, 5, 6))
+
+	fmt.Println(add_two(1, 2, 3))
+
+	fmt.Println(split(17))
+
+	x, y := split(17)
+
+	fmt.Println(x)
+	fmt.Println(y)
+
+	var i int
+	fmt.Println(i, c, python, java)
 }
 
 func add(x int, y int) int {
 	return x + y
+}
+
+func add_many(x, y, z, a, b, c int) int {
+	return x + y + z + a + b + c
+}
+
+func add_two(x, y, z int) (int, int, int, string) {
+	return x + 2, y + 2, z + 2, "Finished adding 2"
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
