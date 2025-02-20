3bec8599528ad5d6195de398f781a26f0c9ef709
Scott Edwards <scotteza@gmail.com>
2025-02-20 15:16:11 +0000

    :earn about numeric constants

--- TheBasicsPart2/the_basics_part_2.go
+++ TheBasicsPart2/the_basics_part_2.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func needInt(x int) int           { return x*10 + 1 }
+func needFloat(x float64) float64 { return x * 0.1 }
+
 func main() {
 	var i int = 42
 	var f float64 = float64(i)
@@ -19,4 +27,10 @@ func main() {
 
 	const who string = "World"
 	fmt.Println("Hello", who)
+
+	fmt.Println(needInt(Small))
+	// This overflows
+	// fmt.Println(needInt(Big))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
