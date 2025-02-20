6cd4ac08e07435f6b3e386deccb1776e318553a9
Scott Edwards <scotteza@gmail.com>
2025-02-20 16:08:09 +0000

    Learn about flow control

--- FlowControl/flow_control.go
+++ FlowControl/flow_control.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	sum := 0
@@ -15,6 +22,36 @@ func main() {
 	}
 	fmt.Println(sum)
 
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a number: ")
+	text, _ := reader.ReadString('\n')
+	stripped := strings.TrimSpace(text)
+	fmt.Printf("You entered %v\n", stripped)
+	someInt, err := strconv.Atoi(stripped)
+	if err != nil {
+		fmt.Println("Error converting string to integer")
+		return
+	}
+	switch someInt % 10 {
+	case 0:
+		fmt.Println("The number is divisible by 10")
+	default:
+		fmt.Println("The number is not divisible by 10")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	case "windows":
+		fmt.Println("Windows.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+}
+
+func infiniteLoop(sum int) {
 	// Infinite loop
 	for {
 		sum++
